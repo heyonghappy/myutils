2663fdefc0aa81a48f3af44be9eb155a51dc2b56
和涌 <heyong@gisiinfo.com>
2018-06-02 16:46:06 +0800

    exports

--- my_utils.js
+++ my_utils.js
@@ -1,17 +1,25 @@
+/**
+ * 判断类型
+ * @param {要判断的} obj 
+ * @param {类型，必须为字符串} typeString 
+ */
 var _isSomeType = function (obj, typeString) { 
     if(arguments.length!=2||typeof typeString !=='string')  return false;
     return Object.prototype.toString.call(obj).slice(8,-1).toLowerCase() === String(typeString).toLowerCase()
 }
 
+/**
+ * 输入的搜索内容
+ * @param {输入的搜索内容} search_term 
+ */
 
 var search_pattern = function(search_term){
-
     var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？%+_]", "g");
     var reg_str = /./;
     reg_str.compile(search_term.replace(/\\/g, '\\\\').replace(pattern, function (x) { return "\\" + x }))
     return reg_str;
 }
-moudle.exports={
+exports={
     _isSomeType:_isSomeType,
     search_pattern:search_pattern
 }
